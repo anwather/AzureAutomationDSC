842fb104fb59d477ebbc7919bca3559d1aa9e5e8
Anthony Watherston <anwather@users.noreply.github.com>
2016-08-19 15:30:35 +1000

    Update ImportCompile.ps1

--- ImportCompile.ps1
+++ ImportCompile.ps1
@@ -7,6 +7,7 @@ $ConfigData = @{
         @{
             NodeName = "*"
             PSDscAllowPlainTextPassword = $True
+            PSDscAllowDomainUser = $True
          },
         @{
             NodeName = "Examplefile"
