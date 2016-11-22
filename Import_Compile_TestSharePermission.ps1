065d575e73d2bf7b742fe1f17022e5e3fcd8e974
Anthony Watherston <anwather@microsoft.com>
2016-11-22 11:31:09 +1100

    Added new configuuration

---
+++ Import_Compile_TestSharePermission.ps1
@@ -0,0 +1,27 @@
+﻿$resourceGroupName = "AA-Automation"
+$automationAccountName = "AADSC-Full"
+$automationAccount = Get-AzureRmAutomationAccount -ResourceGroupName $resourceGroupName -Name $automationAccountName
+
+$ConfigData = @{
+    AllNodes = @(
+        @{
+            NodeName = "*"
+            PSDscAllowPlainTextPassword = $True
+            PSDscAllowDomainUser = $True
+         },
+        @{
+            NodeName = "CMTrace"
+         }
+    )
+}
+
+Import-AzureRmAutomationDscConfiguration -SourcePath "C:\Source\AzureAutomationDSC\TestSharePermission.ps1" `
+                                         -Published `
+                                         -ResourceGroupName $resourceGroupName `
+                                         -AutomationAccountName $automationAccountName `
+                                         -Force `
+                                         -Verbose
+
+$automationAccount | Start-AzureRmAutomationDscCompilationJob -ConfigurationName TestSharePermission -ConfigurationData $ConfigData -Verbose
+
+#$automationAccount | Get-AzureRmAutomationDscOnboardingMetaconfig -OutputFolder C:\Temp
---
+++ TestSharePermission.ps1
@@ -0,0 +1,32 @@
+﻿Configuration TestSharePermission
+{
+
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
+
+    $Credential = Get-AutomationPSCredential -Name "SCCMAdminAccount"
+
+    Node CMTrace
+        {
+
+        File TempFolder
+            {
+                Ensure = "Present"
+				Type = "Directory"
+				DestinationPath = "C:\Temp"
+			}
+        
+        File CMTrace
+            {
+                SourcePath = "\\Sccm-cm01\c$\Program Files\Microsoft Configuration Manager\tools\CMTrace.exe"
+                DestinationPath = "C:\Temp\"
+                Credential = $Credential
+            }
+
+        }
+
+}
+
+
+
+
+
